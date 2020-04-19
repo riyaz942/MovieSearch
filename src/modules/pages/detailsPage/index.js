8a4beeebf5b4042487760b5f0ed8670aee3aef1d
Riyaz <ahmed.riyaz94@gmail.com>
2020-04-19 19:23:35 +0530

    designed details page

--- src/modules/pages/detailsPage/index.js
+++ src/modules/pages/detailsPage/index.js
@@ -5,7 +5,7 @@ import { bindActionCreators } from "redux";
 import { getMovieDetails } from './redux/actions';
 import InitialPageLoader from 'Components/initialPageLoader';
 
-const DetailsPage = ({ match, history, getMovieDetails }) => {
+const DetailsPage = ({ match, history, getMovieDetails, movieDetails }) => {
   const id = match.params.id;
   const onClickBack = () => history.goBack()
 
@@ -17,7 +17,26 @@ const DetailsPage = ({ match, history, getMovieDetails }) => {
       >
         <div className={styles.scrolling_container}>
           <div className={styles.content_container}>
-            
+            <img src={movieDetails.Poster} className={styles.image} />
+
+            <div className={styles.movie_title}>{movieDetails.Title}</div>
+            <div className={styles.sub_title}>{`${movieDetails.Year} | ${movieDetails.Rated} | ${movieDetails.Runtime}`}</div>
+
+            <div className={styles.plot}>{movieDetails.Plot}</div>
+
+            <div className={styles.split_container}>
+              <div className={styles.title}>Director:</div>
+              <div className={styles.description}>{movieDetails.Director}</div>
+            </div>
+
+            <div className={styles.split_container}>
+              <div className={styles.title}>Writer:</div>
+              <div className={styles.description}>{movieDetails.Writer}</div>
+            </div>
+            <div className={styles.split_container}>
+              <div className={styles.title}>Actors:</div>
+              <div className={styles.description}>{movieDetails.Actors}</div>
+            </div>
           </div>
         </div>
       </InitialPageLoader>
