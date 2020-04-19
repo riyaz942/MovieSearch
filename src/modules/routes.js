05b30e82b8376c78d5f71795cc7303e1050ef869
Riyaz <ahmed.riyaz94@gmail.com>
2020-04-19 19:59:37 +0530

    implemented lazy loading

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
+import React, { Suspense } from 'react';
 import { Switch, Route } from "react-router-dom";
-import ListingPage from 'Pages/listingPage';
-import DetailsPage from 'Pages/detailsPage';
+
+const ListingPage = React.lazy(() => import('Pages/listingPage'));
+const DetailsPage = React.lazy(() => import('Pages/detailsPage'));
 
 const Routes = () => {
+  /*
+    Which everpage it opens first, this will load the other page in the background,
+    if the user navigates later to that page the use will already have it loaded in the browser
+  */
+  import("Pages/listingPage").then(()=>{});
+  import("Pages/detailsPage").then(()=>{});
+
   return (
-    <Switch>
-      <Route exact path="/" component={ListingPage} />
-      <Route exact path="/details/:id?" component={DetailsPage} />
-    </Switch>
+    <Suspense fallback={<div>Loading</div>}>
+      <Switch>
+        <Route exact path="/" component={ListingPage} />
+        <Route exact path="/details/:id?" component={DetailsPage} />
+      </Switch>
+    </Suspense>
   )
 }
 
