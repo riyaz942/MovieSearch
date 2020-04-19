1d2eeb76633ece4271b79655fd7de1f941777c66
Riyaz <ahmed.riyaz94@gmail.com>
2020-04-19 18:54:18 +0530

    implemented details page

--- src/modules/pages/detailsPage/index.js
+++ src/modules/pages/detailsPage/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import styles from './index.module.scss';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { getMovieDetails } from './redux/actions';
+import InitialPageLoader from 'Components/initialPageLoader';
 
-const DetailsPage = ({ match }) => {
+const DetailsPage = ({ match, history, getMovieDetails }) => {
   const id = match.params.id;
+  const onClickBack = () => history.goBack()
 
   return (
-    <div>
-      {id}
+    <div className={styles.container}>
+      <div className={styles.back_button} onClick={onClickBack}>BACK</div>
+      <InitialPageLoader
+        initialPageApi={() => getMovieDetails(id)}
+      >
+        <div className={styles.scrolling_container}>
+          <div className={styles.content_container}>
+            
+          </div>
+        </div>
+      </InitialPageLoader>
     </div>
   )
 }
 
-export default DetailsPage;
+const mapStateToProps = state => {
+  return {
+    movieDetails: state.detailsPageReducer.movieDetails,
+  };
+};
+
+const mapDispathToProps = dispatch => {
+  return {
+    getMovieDetails: bindActionCreators(getMovieDetails, dispatch),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispathToProps
+)(DetailsPage);
